2538ce76901283680f1a4d896195cfc7e9db902f
v-lane <victoria.lane.m@gmail.com>
2024-03-25 18:44:34 -0400

    Add interactable dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,71 @@
 import React, { Component } from "react";
 
+import Loading from "./Loading";
+import Panel from "./Panel";
+
 import classnames from "classnames";
 
-class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+const data = [
+  {
+    id: 1,
+    label: "Total Photos",
+    value: 10
+  },
+  {
+    id: 2,
+    label: "Total Topics",
+    value: 4
+  },
+  {
+    id: 3,
+    label: "User with the most uploads",
+    value: "Allison Saeng"
+  },
+  {
+    id: 4,
+    label: "User with the least uploads",
+    value: "Lukas Souza"
+  }
+];
 
-    return <main className={dashboardClasses} />;
+class Dashboard extends Component {
+  state = {
+    loading: false,
+    focused: null
+  };
+
+  selectPanel(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+  }
+
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    const panels = (this.state.focused ?
+      data.filter(panel => this.state.focused === panel.id) :
+      data)
+      .map(panel => (
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={event => this.selectPanel(panel.id)}
+        />
+      ));
+
+    return (
+      <main className={dashboardClasses}>
+        {panels}
+      </main>);
   }
 }
 
